294d6f84ee5534127eefa91ca995b227cc9b4e2a
alias-rahil <rahil.kabani.4@gmail.com>
2023-07-30 09:42:41 +0000

    refactor: cleanup

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -4,6 +4,14 @@ import {electronAPI} from '@electron-toolkit/preload';
 
 const api = {};
 
+declare global {
+	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+	interface Window {
+		electron: typeof electronAPI;
+		api: typeof api;
+	}
+}
+
 if (process.contextIsolated) {
 	contextBridge.exposeInMainWorld('electron', electronAPI);
 	contextBridge.exposeInMainWorld('api', api);
--- src/preload/types.d.ts
+++
@@ -1,9 +0,0 @@
-import type {ElectronAPI} from '@electron-toolkit/preload';
-
-declare global {
-	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-	interface Window {
-		electron: ElectronAPI;
-		api: unknown;
-	}
-}
